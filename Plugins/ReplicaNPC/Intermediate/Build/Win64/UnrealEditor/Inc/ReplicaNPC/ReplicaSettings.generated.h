0ea56143577e93161af32d4b43444bffaf4b76a4
Alex Jay <cristmas.bel@gmail.com>
2023-10-16 12:55:13 +0300

    ALS and Replica plugins have been added. Created a basic character based on ALS

---
+++ Plugins/ReplicaNPC/Intermediate/Build/Win64/UnrealEditor/Inc/ReplicaNPC/ReplicaSettings.generated.h
@@ -0,0 +1,96 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef REPLICANPC_ReplicaSettings_generated_h
+#error "ReplicaSettings.generated.h already included, missing '#pragma once' in ReplicaSettings.h"
+#endif
+#define REPLICANPC_ReplicaSettings_generated_h
+
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_SPARSE_DATA
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_RPC_WRAPPERS
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUReplicaSettings(); \
+	friend struct Z_Construct_UClass_UReplicaSettings_Statics; \
+public: \
+	DECLARE_CLASS(UReplicaSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/ReplicaNPC"), NO_API) \
+	DECLARE_SERIALIZER(UReplicaSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesUReplicaSettings(); \
+	friend struct Z_Construct_UClass_UReplicaSettings_Statics; \
+public: \
+	DECLARE_CLASS(UReplicaSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/ReplicaNPC"), NO_API) \
+	DECLARE_SERIALIZER(UReplicaSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UReplicaSettings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UReplicaSettings) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UReplicaSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UReplicaSettings); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UReplicaSettings(UReplicaSettings&&); \
+	NO_API UReplicaSettings(const UReplicaSettings&); \
+public:
+
+
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UReplicaSettings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UReplicaSettings(UReplicaSettings&&); \
+	NO_API UReplicaSettings(const UReplicaSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UReplicaSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UReplicaSettings); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UReplicaSettings)
+
+
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_12_PROLOG
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_SPARSE_DATA \
+	FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_RPC_WRAPPERS \
+	FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_INCLASS \
+	FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_SPARSE_DATA \
+	FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_INCLASS_NO_PURE_DECLS \
+	FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> REPLICANPC_API UClass* StaticClass<class UReplicaSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_ReplicaSample_Plugins_ReplicaNPC_Source_ReplicaNPC_Public_ReplicaSettings_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
