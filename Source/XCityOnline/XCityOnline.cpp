73ec432dbcac85619d840f66b4d9f3d80349418d
PavelSamura <pasha.splav22@gmail.com>
2023-10-25 23:27:01 +0700

    Pavel Dok
    delete non usable files

--- Source/XCityOnline/XCityOnline.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "XCityOnline.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, XCityOnline, "XCityOnline" );
